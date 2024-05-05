d181d70fb5ce6f7c40f269622ffabaed2e036d10
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2024-05-05 04:42:44 -0400

    Update JCR Soft Choice.py

--- JCR Soft Choice.py
+++ JCR Soft Choice.py
@@ -1,12 +1,20 @@
+# J.C.R Soft~Choice File Menu Python program example:
+
+# Created by Joseph C. Richardson, GitHub.com
+
 # You will need to import your own files to correctly make this Python
 # file menu program example work. You can also use some of my program
 # examples to create a working Python file menu of your very own.
-# I created this file menu and its files associated with it, using def functions().
-# This Python file menu example shows how to create modules and
+# I created this file menu and its files associated with it, using def
+# functions(). This Python file menu example shows how to create modules and
 # how to import them into your very own file menu. Note: you must store
 # all your files within the very same folder you create your file menu in.
 # If you don't do such, the file menu won't be able to find the files you
-# want in your file menue. See my file menu folder image below:
+# want in your file menu. See my file menu folder image below. Also note
+# the little yellow folder you see at the very top makes itself. You must
+# leave this folder where it is and don't play with the files inside it. These
+# are binary files for the links to the files in your file menu Python program
+# example. 
 
 import os,time,math
 
